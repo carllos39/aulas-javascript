101cac0ad3733a791e5b675fbbf2fdcc5463f122
carllos39 <carllos2005@outlook.com.br>
2023-10-05 15:03:30 -0300

    js

---
+++ js/01-sintaxe.js
@@ -0,0 +1,19 @@
+console.log("programador web");
+console.log('Senac Penha');
+// Comentario
+// Manipulacão de dados
+// Variáveis 
+let curso="HTML CSS3";
+let ano = 2023;
+// Constantes
+const mediaMinima=7;
+// Saida de dados
+console.log(curso);
+console.log(mediaMinima);
+console.log(ano);
+
+// Estou fazendo o curso  de HTML5 e CSS3 neste ano de 2023.
+// teplante String/Literal:saida uhsando crese e ${}
+console.log(`Estou fazendo curso de ${curso} neste ano de ${ano}.`);
+// Concatenação(forma mais antiga de saida ,mais ainda válida)
+console.log("Estou fazendo o curso de" + curso+"neste ano de"+ ano+".");
