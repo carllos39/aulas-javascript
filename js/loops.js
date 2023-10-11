a207f8299c95b18ac5f45dd87983cc905a9fe857
carllos39 <carllos2005@outlook.com.br>
2023-10-11 16:03:34 -0300

    loop

---
+++ js/loops.js
@@ -0,0 +1,16 @@
+//Comandos de repetição,instrucões para realizar ações por uma quantidade  especifica de vazes.
+
+//Comandos tradicionais:
+//while -> enquanto
+//do/while -> faça /enquanto
+//for -> para
+//Obs: normalmente o loop é controlado através  de uma variável  contadora.
+
+let contador=1;
+while(contador <= 5){
+    console.log(`Valor de contador : ${contador}`);
+
+    contador ++;
+
+}
+
