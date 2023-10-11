62a72fbe9798ebf4eb60bb599cc82a74e496ed78
carllos39 <carllos2005@outlook.com.br>
2023-10-11 14:56:52 -0300

    lógicos

--- js/operadores-logicos.js
+++ js/operadores-logicos.js
@@ -4,7 +4,7 @@ const limiteDeFaltas=10;
 
 //Resultado do aluno fectício
 let aluno="Carlos";
-let mediaFinal=5.5;
+let mediaFinal=4.5;
 let faltas=10;
 let resultado;
 //Operador E ou End &&
@@ -15,15 +15,35 @@ resultado="aprovado!";
     //resultado="aprovado!";
 //}
 
-if(mediaFinal >= mediaMinima && faltas <= limiteDeFaltas){
-   resultado="APROVADO!";
-}else if(faltas > limiteDeFaltas){
+//if(mediaFinal >= mediaMinima && faltas <= limiteDeFaltas){
+  // resultado="APROVADO!";
+//}else if(faltas > limiteDeFaltas){
     resultado="REPROVADO POR FALTAS!";
-}else{
+//}else{
     resultado="REPROVADO!";   
-}
+//}
 
+if(faltas > limiteDeFaltas){
+    resultado="Reprovado por faltas";
+
+}else if(mediaFinal >= mediaMinima){
+    resultado="Aprovado";
+
+}else{
+    resultado="Reprovado";
+}
 
 console.log(`O aluno ${aluno} está ${resultado}`);
 console.log(`A média do aluno: ${mediaFinal} `);
 console.log(`Faltas do aluno: ${faltas}`);
+
+console.log("------------------------------------------");
+//Operador lógico || ou
+
+let diaDasSemana="domingo";
+if(diaDasSemana == "sabado" || diaDasSemana=="domingo"){
+    console.log("Final de semana");
+
+}else{
+    console.log("Vá trabalhar...");    
+}
