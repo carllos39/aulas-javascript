a4b653cc361488f5b402c48cb098854b62a7cc21
carllos39 <carllos2005@outlook.com.br>
2023-10-11 00:11:40 -0300

    correção

--- js/exercicio01.js
+++ js/exercicio01.js
@@ -1,13 +1,13 @@
 //Habilitando o modo estrito do JS
 "use strict";
-let nota1=2;
+let nota1=5;
 let nota2=7;
 let media=(nota1+nota2)/2;
 console.log(media);
-//if(media>=7){
-   // console.log(`A média é ${media} APROVADO! `);
-//}else if(media>=5){
-   // console.log(`A média é ${media} RECUPERAÇÃO!`);
-//}else{
-   // console.log(`A média é ${media} REPROVADO! `);
-//}
+if(media>=7){
+    console.log(`A média é ${media} APROVADO! `);
+}else if(media>=5){
+    console.log(`A média é ${media} RECUPERAÇÃO!`);
+}else{
+    console.log(`A média é ${media} REPROVADO! `);
+}
