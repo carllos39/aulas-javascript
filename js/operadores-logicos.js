4c5b82bca9af5d74b038cd5438cfae2cb81cccb8
carllos39 <carllos2005@outlook.com.br>
2023-10-11 14:37:55 -0300

    condicional

--- js/operadores-logicos.js
+++ js/operadores-logicos.js
@@ -4,16 +4,26 @@ const limiteDeFaltas=10;
 
 //Resultado do aluno fectício
 let aluno="Carlos";
-let mediaFinal=8.5;
-let faltas=8;
+let mediaFinal=5.5;
+let faltas=10;
 let resultado;
 //Operador E ou End &&
 
-if(mediaFinal >=mediaMinima && faltas < limiteDeFaltas){
+//if(mediaFinal >=mediaMinima && faltas < limiteDeFaltas){
 resultado="aprovado!";
+//}else{
+    //resultado="aprovado!";
+//}
+
+if(mediaFinal >= mediaMinima && faltas <= limiteDeFaltas){
+   resultado="APROVADO!";
+}else if(faltas > limiteDeFaltas){
+    resultado="REPROVADO POR FALTAS!";
 }else{
-    resultado="aprovado!";
+    resultado="REPROVADO!";   
 }
+
+
 console.log(`O aluno ${aluno} está ${resultado}`);
 console.log(`A média do aluno: ${mediaFinal} `);
 console.log(`Faltas do aluno: ${faltas}`);
