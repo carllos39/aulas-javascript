2d772af98a36d3c72d0b456ccaa1dfbfe0444158
carllos39 <carllos2005@outlook.com.br>
2023-10-17 14:53:56 -0300

    funções

---
+++ js/funcoes.js
@@ -0,0 +1,7 @@
+"use strict";
+//Sobre funções
+//Funções são blocos de códigos  que  podem  ser executados  e reaproveitados em praticamente qualquer  lugar  de  uma  aplicação /site.
+//Usar funções tambem é benefico do ponto de vista de organização de códigos.
+//O js já possui centenas  de funções prontas:
+//Exemlpo: log(), alert(),prompt(),toFixed(),etc...
+
