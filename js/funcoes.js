774d1a41466a499f6926af7a6923b557c12f2aaf
carllos39 <carllos2005@outlook.com.br>
2023-10-17 15:19:22 -0300

    função soma

--- js/funcoes.js
+++ js/funcoes.js
@@ -4,4 +4,32 @@
 //Usar funções tambem é benefico do ponto de vista de organização de códigos.
 //O js já possui centenas  de funções prontas:
 //Exemlpo: log(), alert(),prompt(),toFixed(),etc...
+//Como escrever funções em JS?
+//Forma1: função anômima
+const exemplo1=function(){
+    console.log("Função anônima");
 
+}
+//Forma2: Função nomeada/declarada
+
+function exemplo2(){
+    console.log("Função nomeada/declarada");
+}
+
+//Forma3:arrow function
+const exemplo3=()=>{
+    console.log("Arrow Function");
+}
+//Chamada de função
+exemplo1();
+exemplo2();
+exemplo3();
+//Exemplos diversos
+//Função com parâmetro e retorno de dados/resultados
+function somar(valor1,valor2){
+     return valor1 + valor2;
+
+}
+console.log(somar(10,20));
+console.log(somar(15,50));
+console.log(somar(30,47.8));
