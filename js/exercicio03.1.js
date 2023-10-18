0be83b29c12cd5a147144745c9c6ba1bf1bfde58
carllos39 <carllos2005@outlook.com.br>
2023-10-18 14:57:12 -0300

    fatoração

--- js/exercicio03.1.js
+++ js/exercicio03.1.js
@@ -1,12 +1,22 @@
-let valorDaCotacao=5.3;
-let valorEmDolar=1500;
+let valorDaCotacao=5.03;
+let valorEmDolar=2500;
 
 function converteMoeda(valor,cotacao){
     return valor*cotacao;
     
 }
 
+function formatarMoeda(valor,local,moeda){
+    let opcoes={
+        style:"currency",
+        currency:moeda
+    };
+    return valor.toLocaleString(local,opcoes);
+    }
+
 let valorConvertido=converteMoeda(valorDaCotacao,valorEmDolar);
 
-console.log(valorEmDolar);
-console.log(valorConvertido);
+//console.log(valorEmDolar);
+//console.log(valorConvertido);
+console.log(formatarMoeda(valorEmDolar,"en","USD"));
+console.log(formatarMoeda(valorConvertido,"pt-br","BRL"));
